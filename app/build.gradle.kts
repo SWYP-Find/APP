7461ceca737cfca8a17dcfc00225d78013698830
jessy <yaeunjess@gmail.com>
2026-04-16 19:33:47 +0900

    🔖 버전 3로 업데이트

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         applicationId = "com.picke.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
+        versionCode = 3
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
